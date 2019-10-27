547a528c5f9ca625a26bc02c39d0fde7e80b1dfc
Oscar Sánchez <ojsm45@gmail.com>
2019-10-26 21:26:16 -0300

    finish proptypes validation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import ListaNoticias from './components/ListaNoticias';
+import Formulario from './components/Formulario';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    category: 'general',
+    noticias: []
+  };
+
+  componentDidMount() {
+    this.consultarNoticias();
+  }
+
+  cambiarCategoria = e => {
+    const category = e.target.value;
+
+    this.setState({
+      category
+    });
+
+    this.consultarNoticias(category);
+  };
+
+  consultarNoticias = async categoria => {
+    const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${
+      categoria ? categoria : this.state.category
+    }&apiKey=6c1c1dfcb4a943c7bf481bc628b80153`;
+
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+
+    this.setState({
+      noticias: noticias.articles
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Header titulo='Noticias React API' />
+        <div className='container white contenedor-noticias'>
+          <Formulario handleChange={this.cambiarCategoria} />
+          <ListaNoticias noticias={this.state.noticias} />
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
